5d691f54a9a8d0ae0db3f8aeeb5daf7a1fca2557
Nandan B Deshamukh <158804961+nandan-byte@users.noreply.github.com>
2025-06-14 22:39:30 +0530

    Update pattern5.cpp

--- 3. Patterns/pattern5.cpp
+++ 3. Patterns/pattern5.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 void printPattern5(int n){
-for(int i=n;i>=0;i--){
-  for(int j=0;j<=i;j++){
+for(int i=n;i>=1;i--){
+  for(int j=1;j<=i;j++){
     cout<<"*";
   }
   cout<<endl;
