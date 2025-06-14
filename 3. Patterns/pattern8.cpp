221112543c2d4544c85d1353002f5bfb64a02b57
Nandan B Deshamukh <158804961+nandan-byte@users.noreply.github.com>
2025-06-14 23:26:27 +0530

    Update pattern8.cpp

--- 3. Patterns/pattern8.cpp
+++ 3. Patterns/pattern8.cpp
@@ -1,1 +1,23 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+void printPattern8(int n){
+  for(int i=n;i>0;i--){
+    for(int j=0; j<n-i; j++){
+      cout<<" ";
+    }
+    for(int j=0;j<2*i-1;j++){
+      cout<<"*";
+    }
+    for(int j=0;j<n-i;j++){
+      cout<<" ";
+    }
+    cout<<endl;
+  }
+}
+int main(){
+  int rows;
+  cout<<"Enter the number of rows: ";
+  cin>>rows;
+  printPattern8(rows);
+  return 0;
+}
